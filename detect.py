bd9f5f74ba3769597bb0aa035af2843db6de5fef
c1ph3rr <suraj.subbarao96@gmail.com>
2018-08-31 18:39:26 +0530

    gui

--- detect.py
+++ detect.py
@@ -1,15 +1,38 @@
 import pickle
 import spam
+from tkinter import *
 
 with open('model.pkl', 'rb') as f:
     clf = pickle.load(f)
 
-stat = True
-while stat:
-    user_input = input('input \n')
-    if user_input == 'exit':
-        stat = False
-    else:
-        features = spam.create_features(user_input)
-        #print(features)
-        print(clf.classify(features))
+def detect():
+    user_input = input1.get()
+    features = spam.create_features(user_input)
+    answer.config(text=clf.classify(features))
+
+root = Tk()
+root.title('Spam detector')
+root.geometry('400x300')
+root.resizable(width=False, height=False)
+
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+label1 = Label(topFrame, text='Enter the word: ')
+label1.pack()
+
+input1 = Entry(topFrame)
+input1.pack()
+
+button1 = Button(topFrame, text='Detect', command=detect)
+button1.pack()
+
+button2 = Button(bottomFrame, text='Quit',fg='red', command=quit)
+button2.pack()
+
+answer = Label(topFrame, text='')
+answer.pack()
+
+root.mainloop()
